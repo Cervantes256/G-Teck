7631a56875b1f54f2edecbb87a2708fed8b96f30
Javier Cervantes Carmona <cervantescarmonaj@gmail.com>
2023-10-03 03:11:03 -0600

    index on main: 246df3e Initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/*Definicion de variables globales
+ROOT_PATH: Esta constante representa la ruta absoluta del 
+directorio en el que se encuentra el archivo PHP actual.
+*/
+define('ROOT_PATH', realpath(dirname(__FILE__)));
+#BASE_URL: Contiene la URL base del sitio web.
+define('BASE_URL', 'http://localhost/Encuesta/');
+
+//Fecha y hora de mexico
+date_default_timezone_set('America/Mexico_City');
