4d5ca1a9235d470c4dedcf6e5c60d65f86f03600
Javier Cervantes Carmona <cervantescarmonaj@gmail.com>
2023-10-12 16:06:50 -0600

    Nuevo cambio

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ directorio en el que se encuentra el archivo PHP actual.
 */
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 #BASE_URL: Contiene la URL base del sitio web.
-define('BASE_URL', 'https://github.com/Cervantes256/G-Teck/blob/main/');
+define('BASE_URL', 'https://github.com/Cervantes256/G-Teck/');
 
 //Fecha y hora de mexico
 date_default_timezone_set('America/Mexico_City');
